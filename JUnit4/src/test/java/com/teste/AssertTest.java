d541dc3189130eb43cd03cb11ac38ada38ab8634
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-11 15:13:42 -0400

    Mocks, Asserts e TDD

--- JUnit4/src/test/java/com/teste/AssertTest.java
+++ JUnit4/src/test/java/com/teste/AssertTest.java
@@ -48,4 +48,5 @@ public class AssertTest {
 		Integer aNumber = Integer.valueOf(768);
 		assertSame(aNumber,aNumber);
 	}
+	
 }
--- JUnit4/src/test/java/com/teste/CalculadoraTest.java
+++ JUnit4/src/test/java/com/teste/CalculadoraTest.java
@@ -1,6 +1,9 @@
 package com.teste;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
@@ -11,6 +14,18 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma = calc.somar("1+1+3");
 		assertEquals(5, soma);
+	}
+	
+	@Test
+	public void testeSomarComMock() {
+		
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1+2")).thenReturn(10);
+		
+		int resultado = calculadora.somar("1+2");
+		System.out.println(resultado);
+		assertEquals(10.0, resultado);
 	}
 
 }
