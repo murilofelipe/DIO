bb20f9b43146c0750078d784ef4e4523b148bafb
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-12 21:10:22 -0400

    Introdução ao Spring Boot

---
+++ springboot/src/main/java/com/dio/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dio.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String helloMessage() {
+		return "Hello DIO";
+	}
+}
