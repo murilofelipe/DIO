31cde4a00ce474bf0bc8a8ce8f56d1ae92c95bc7
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-13 15:09:36 -0400

    Configuração de variável de ambiente

--- springboot/src/main/java/com/dio/springboot/controller/AppController.java
+++ springboot/src/main/java/com/dio/springboot/controller/AppController.java
@@ -10,8 +10,16 @@ public class AppController {
 	@Value("${app.message}")
 	private String appMessage;
 	
+	@Value("${ENV_DB_URL:NENHUMA}")
+	private String dbEnvironmentVariable;
+	
 	@GetMapping("/")
 	public String getAppMessage() {
 		return appMessage;
+	}
+	
+	@GetMapping("envVariable")
+	public String getEnvironmentVariable() {
+		return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
 	}
 }
